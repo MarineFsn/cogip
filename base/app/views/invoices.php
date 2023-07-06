6b5cb7c8e11c42432d86ffa9d2a2dcd33813c33e
LeShib <dibernardonikko@gmail.com>
2023-07-06 11:44:35 +0200

    update structure

---
+++ base/app/views/invoices.php
@@ -0,0 +1,13 @@
+<?php
+include "header.php";
+?>
+
+<main>
+    <?php
+    echo 'invoices';
+    ?>
+</main>
+<script src="./base/public/assets/js/invoices.js"></script>
+<?php
+include "footer.php";
+?>
