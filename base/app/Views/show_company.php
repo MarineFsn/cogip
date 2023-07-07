a483f96ff1cee41ce4b2ad3df0cb9e69bdeee789
Loic Lion <loic.lion.contact@gmail.com>
2023-07-07 16:01:21 +0200

    début show company

--- base/app/Views/show_company.php
+++ base/app/Views/show_company.php
@@ -1,7 +1,26 @@
 <?php
 include "header.php";
 ?>
-
+<main>
+    <section class="container__contact">
+        <div class="container__contact__info">
+            <div class="container__contact__info__name">
+                <h5>Nom de la compagnie</h5>
+                <div class="container__contact__info__name__yellow"></div>
+            </div>
+            <p>Name:</p>
+            <p>TVA:</p>
+            <p>Country:</p>
+            <p>Type:</p>
+        </div>
+    </section>
+    <hr>
+    <section>
+        <h5>Contact People</h5>
+        <div></div>
+    </section>
+</main>
+<script src="/cogip/base/public/assets/js/invoices.js"></script>
 
 <?php
 include "footer.php";
