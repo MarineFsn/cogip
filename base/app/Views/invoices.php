e6606568ba492a138f47f58a098082d0ee9320f3
Benja <benjaminmayeur01@gmail.com>
2023-07-07 14:01:53 +0200

    start home page

--- base/app/Views/invoices.php
+++ base/app/Views/invoices.php
@@ -1,14 +1,42 @@
 <?php
 include "header.php";
+include "../../Controllers/invoiceController.php";
 ?>
 
 <main>
-    test test test
-    <?php
-    echo 'invoices';
-    ?>
+    <section class="container__table">
+        <h3>All invoices </h3>
+        <div class="container__table__yellow__rectangle">
+        <input type="text" id="searchbar" name="searchbar" placeholder="Search company" required>
+    
+            <?php
+                $controllercontact = new invoiceController();
+                $invoices = $controllercontact->getinvoices();
+            ?>
+            <table class="table__info">
+                <thead class="table__info__thead">
+                    <tr class="table__info__thead__tr">
+                        <th>Invoice number</th>
+                        <th>Due dates</th>
+                        <th>Company</th>
+                        <th>Created at</th>
+                    </tr>
+                </thead>
+                <tbody class="table__info__tbody">
+                    <?php
+                        foreach ($invoices as $invoice) {
+                            echo "<td>" . $invoice->ref;
+                            echo "<td>" . $company->update_date ;
+                            echo "<td>" . $invoice->company;
+                            echo "<td>" . $invoice->created_at;
+                        }
+                    ?>
+                </tbody>
+            </table>
+        </div>
+    </section>
 </main>
-<script src="./base/public/assets/js/invoices.js"></script>
+<script src="/projects/cogip/base/public/assets/js/invoices.js"></script>
 <?php
 include "footer.php";
 ?>
