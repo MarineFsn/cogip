d39e19046d081efd1b455d7468ef9a10127575de
Loic Lion <loic.lion.contact@gmail.com>
2023-07-10 14:23:17 +0200

    fix scss

---
+++ base/app/Views/show_invoices.php
@@ -0,0 +1,22 @@
+<?php
+include "header.php";
+?>
+<main>
+    <section class="container__contact">
+        <div class="container__contact__info">
+            <div class="container__contact__info__name">
+                <h5>Numero invoice</h5>
+                <div class="container__contact__info__name__yellow"></div>
+            </div>
+            <p>Invoice number:</p>
+            <p>Due dates:</p>
+            <p>Created at:</p>
+            <p>Company:</p>
+        </div>
+    </section>
+</main>
+<script src="/cogip/base/public/assets/js/invoices.js"></script>
+
+<?php
+include "footer.php";
+?>
